9387d8452414043c7da19d580bc5d21a79915e4c
maroua <marouaamal2002@gmail.com>
2023-05-15 01:32:01 +0100

    edited the homepage

--- Homepage.py
+++ Homepage.py
@@ -6,56 +6,67 @@ from PIL import Image,ImageTk
 
 # display the characteristics of the graph
 def display_graph_characteristics(G):
-        # Création de la fenêtre
-        fenetre = tk.Toplevel()
-        fenetre.title("Caractéristiques du graphe")
-        # Nombre de sommets
-        nb_sommets = G.number_of_nodes()
-        label_sommets = tk.Label(fenetre, text="Nombre de sommets : {}".format(nb_sommets))
-        label_sommets.pack()
-        # Nombre d'arêtes
-        nb_aretes = G.number_of_edges()
-        label_aretes = tk.Label(fenetre, text="Nombre d'arêtes : {}".format(nb_aretes))
-        label_aretes.pack()
-        # Liste des sommets
-        sommets = G.nodes()
-        label_sommets_liste = tk.Label(fenetre, text="Liste des sommets : {}".format(sommets))
-        label_sommets_liste.pack()
-        # Liste des arêtes
-        aretes = G.edges()
-        label_aretes_liste = tk.Label(fenetre, text="Liste des arêtes : {}".format(aretes))
-        label_aretes_liste.pack()
-        #densité
-        densite = nx.density(G)
-        label_densite = tk.Label(fenetre, text="Densite : {}".format(densite))
-        label_densite.pack()
+    # Create a tkinter window
+    window = tk.Tk()
 
-        fenetre.mainloop()
+    # Calculate and display the number of nodes and edges in the graph
+    num_nodes = G.number_of_nodes()
+    num_edges = G.number_of_edges()
+    label1 = tk.Label(window, text="Number of nodes: " + str(num_nodes))
+    label2 = tk.Label(window, text="Number of edges: " + str(num_edges))
 
-def create_normal_graph():
-    
+    # Calculate and display the average degree of the graph
+    avg_degree = sum(dict(G.degree()).values()) / num_nodes
+    label3 = tk.Label(window, text="Average degree: " + str(avg_degree))
 
-    def create_matrix():
-        
+    # Calculate and display the diameter of the graph
+    try:
+        diameter = nx.diameter(G)
+        label4 = tk.Label(window, text="Diameter: " + str(diameter))
+    except nx.exception.NetworkXError:
+        label4 = tk.Label(window, text="Diameter: Not connected")
+
+    # Calculate and display the clustering coefficient of the graph
+    clustering_coefficient = nx.average_clustering(G)
+    label5 = tk.Label(window, text="Clustering coefficient: " + str(clustering_coefficient))
+
+    # Add the labels to the window
+    label1.pack()
+    label2.pack()
+    label3.pack()
+    label4.pack()
+    label5.pack()
+
+    # Run the tkinter window
+    window.mainloop()
+
+# create a normal graph
+
+def create_matrix():
         # get the values from the input fields
-        try:
-            rows = int(rows_no.get())
-            cols = int(cols_no.get())
-        except ValueError:
-            error_label.configure(text="Entrez que des valeurs entières")
-            return
-        matrix_frame = tk.Frame(window, width=200, height=200, bd=1, relief=tk.SOLID, bg='#CCE5FF')
-        matrix_frame.grid(row=4, column=1, padx=10, pady=10)
+    try:
+        rows = int(rows_no.get())
+        cols = int(cols_no.get())
+    except ValueError:
+        error_label.configure(text="Entrez que des valeurs entières")
+        return
+    
+    # calculate size of matrix frame based on number of rows and columns
+    frame_width = cols * 50
+    frame_height = rows * 30
+    
+    matrix_frame = tk.Frame(window, width=frame_width, height=frame_height, bd=1, relief=tk.SOLID, bg='#CCE5FF')
+    matrix_frame.grid(row=4, column=1, padx=10, pady=10)
 
-        # create matrix of input fields
-        matrix = []
-        for i in range(rows):
-            row = []
-            for j in range(cols):
-                entry = tk.Entry(matrix_frame, width=5)
-                entry.grid(row=i, column=j)
-                row.append(entry)
-            matrix.append(row)
+    # create matrix of input fields
+    matrix = []
+    for i in range(rows):
+        row = []
+        for j in range(cols):
+            entry = tk.Entry(matrix_frame, width=5)
+            entry.grid(row=i, column=j)
+            row.append(entry)
+        matrix.append(row)
         
         # get values from matrix
         def get_matrix_values():
@@ -118,14 +129,12 @@ def create_normal_graph():
             else:
                 error_label.configure(text="Entrez au moins une node dans la matrice")
 
-            # create button to get the characteristics of the graph
-            button_characteristics = tk.Button(window, text="Get Characteristics", command=display_graph_characteristics(G))
-            button_characteristics.grid(row=5, column=4)
-
             # Set the clicked flag to True
             submit_button.clicked = True
-
-
+            # create button to get the characteristics of the graph
+           
+            button_characteristics = tk.Button(window, text="Get Characteristics", command=lambda: display_graph_characteristics(G))
+            button_characteristics.grid(row=6, column=2, pady=10)
 
         submit_button.clicked = True
         submit_button.configure(command=submit_matrix)
@@ -136,30 +145,97 @@ def create_normal_graph():
 
         undirected_button = tk.Button(window, text="Undirected", command=lambda: setattr(undirected_button, "clicked", True))
         undirected_button.grid(row=4, column=4, padx=10)
-        undirected_button.clicked = False    
+        undirected_button.clicked = False
 
-    window = tk.Tk()
-    window.geometry("800x600")
-    window.title("théorie de graphes")
+            
 
-    rows_label=tk.Label(window, text="Nombre de lignes: ").grid(row=0, column=0)
-    cols_label=tk.Label(window, text="Nombre de colonnes:").grid(row=1, column=0)
+# create a weighted graph
+def create_weighted_graph():
 
-    rows_no = tk.StringVar()
-    cols_no = tk.StringVar()
+    # Create an empty graph
+    G = nx.Graph()
 
-    rows_entry = tk.Entry(window, textvariable=rows_no)
-    rows_entry.grid(row=0, column=1)
-    cols_entry = tk.Entry(window, textvariable=cols_no)
-    cols_entry.grid(row=1, column=1)
+    # Define a function to add nodes to the graph
+    def add_node():
+        node_label = node_entry.get()
+        G.add_node(node_label)
+        node_entry.delete(0, tk.END)
 
-    submit_button = tk.Button(window, text="Submit", command= create_matrix)
-    submit_button.grid(row=2, column=1)
+    # Define a function to add edges to the graph
+    def add_edge():
+        source = source_entry.get()
+        target = target_entry.get()
+        G.add_edge(source, target)
+        source_entry.delete(0, tk.END)
+        target_entry.delete(0, tk.END)
 
-    error_label = tk.Label(window)
-    error_label.grid(row=3, column=1)
+    # Define a function to create a dictionary of the graph
+    def create_dict():
+        graph_dict = {}
+        for node in G.nodes:
+            graph_dict[node] = []
+            for neighbor in G.neighbors(node):
+                graph_dict[node].append(neighbor)
+        print(graph_dict)
 
-    window.mainloop()
+    G = nx.Graph()
+    # Display the graph
+    def display_graph():
+        pos = nx.spring_layout(G)
+        nx.draw(G, pos, with_labels=True)
+        # Create a canvas widget and embed the graph in it
+        canvas = FigureCanvasTkAgg(plt.gcf(), master=root)
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=10, column=0, columnspan=3, pady=10)
+        
+        # create button to get the characteristics of the graph
+        button_characteristics = tk.Button(root, text="Get Characteristics", command=lambda: display_graph_characteristics(G))
+        button_characteristics.grid(row=5, column=0)
+
+
+
+
+
+    # Create a tkinter window
+    root = tk.Tk()
+    root.geometry("800x600")
+
+    # Create node entry and button
+    node_label = tk.Label(root, text="Node:")
+    node_label.grid(row=0, column=0)
+    node_entry = tk.Entry(root)
+    node_entry.grid(row=0, column=1)
+    node_button = tk.Button(root, text="Add Node", command=add_node)
+    node_button.grid(row=0, column=2)
+
+    # Create source and target entry and button
+    source_label = tk.Label(root, text="Source:")
+    source_label.grid(row=1, column=0)
+    source_entry = tk.Entry(root)
+    source_entry.grid(row=1, column=1)
+    target_label = tk.Label(root, text="Target:")
+    target_label.grid(row=2, column=0)
+    target_entry = tk.Entry(root)
+    target_entry.grid(row=2, column=1)
+    edge_button = tk.Button(root, text="Add Edge", command=add_edge)
+    edge_button.grid(row=2, column=2)
+
+    # Create create dictionary button
+    dict_button = tk.Button(root, text="Create Dictionary", command=create_dict)
+    dict_button.grid(row=3, column=1)
+
+    # Create display button
+    display_button = tk.Button(root, text="Display Graph", command=display_graph)
+    display_button.grid(row=4, column=1, padx=10, pady=10)
+
+    # Start the tkinter event loop
+    root.mainloop()
+
+
+
+
+    
+
 
 
 # create the window image
@@ -184,13 +260,41 @@ label = tk.Label(window_img, image=photo)
 label.pack()
 
 # add the button of the weighted graph and the normal one
-button_normal = tk.Button(window_img, text="Graphe normal",command=create_normal_graph)
+button_normal = tk.Button(window_img, text="Graphe normal")
 button_normal.pack()
 
-button_weighted = tk.Button(window_img, text="Graphe pondéré")
+button_weighted = tk.Button(window_img, text="Graphe pondéré", command=create_weighted_graph)
 button_weighted.pack()
 
 window_img.mainloop()
+
+
+
+window = tk.Tk()
+window.geometry("800x600")
+window.title("théorie de graphes")
+
+rows_label=tk.Label(window, text="Nombre de lignes: ").grid(row=0, column=0)
+cols_label=tk.Label(window, text="Nombre de colonnes:").grid(row=1, column=0)
+
+rows_no = tk.StringVar()
+cols_no = tk.StringVar()
+
+rows_entry = tk.Entry(window, textvariable=rows_no)
+rows_entry.grid(row=0, column=1)
+cols_entry = tk.Entry(window, textvariable=cols_no)
+cols_entry.grid(row=1, column=1)
+
+submit_button = tk.Button(window, text="Submit", command= create_matrix)
+submit_button.grid(row=2, column=1)
+
+error_label = tk.Label(window)
+error_label.grid(row=3, column=1)
+
+window.mainloop()
+
+
+
 
 
 
